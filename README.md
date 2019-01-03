0a922646a1bb465fd7c6d4d897d4ca5f8fc37acc
Moss <827291427@qq.com>
2019-01-03 18:36:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+echo "# toolBox" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mossbaoo/toolBox.git
+git push -u origin master
+
+edit files
+git commit -m "***"
+git push -u origin master
+
+add files
+git add -A
+git commit -m "***"
+git push -u origin master
---
+++ pages/setup/setup.js
@@ -0,0 +1,26 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    flagArr: ['变瘦变美', '赚个一百万', '绝对不熬夜了', '去一次国外旅行', '世界和平', '暴富！', '每天看书半小时', '和家人一起去旅行', '练个马甲线', '练出八块腹肌', '早睡早起', '脱单！', '高考顺利', '不挂科', '瘦10斤', '学会做饭'],
+    showFlagArr: [],
+  },
+
+  onLoad() {
+    this.showFlag()
+  },
+
+  // 展示随机flag
+  showFlag() {
+    let showFlagArr = [];
+    for(var i=0; i<9; i++) {
+      let index = parseInt(Math.random()*this.data.flagArr.length);
+      showFlagArr.push(this.data.flagArr[index])
+    }
+    this.setData({
+      showFlagArr: showFlagArr
+    })
+  },
+
+})
