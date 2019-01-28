d0f769af0ba8a5c7e5020203ba38228401ce2d62
Moss <827291427@qq.com>
2019-01-28 18:23:27 +0800

    ++

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,13 +9,46 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: true,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     isCollect: false,
     timeName: '',
   },
 
   onShow() {
     this.getTimeName();
+  },
+
+  onLoad() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    app.wxLogin().then(res=>{
+      if(res == '未授权') {
+        this.setData({
+          hasUserInfo: false
+        })
+        wx.hideLoading()
+      }else {
+        this.setData({
+          userInfo: app.globalData.userInfo,
+          hasUserInfo: true
+        })
+        wx.hideLoading()
+      }
+    })
+  },
+
+  // 获取用户信息
+  getUserInfo(e) {
+    console.log(e)
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    } else {
+      console.log('拒绝')
+    }
   },
 
   /**
@@ -55,55 +88,6 @@ Page({
     this.setData({
       timeName: timeName
     })
-  },
-
-  onLoad() {
-    console.log(11)
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      this.setData({
-        hasUserInfo: false
-      })
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      this.setData({
-        hasUserInfo: false
-      })
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-
-  getUserInfo(e) {
-    console.log(e)
-    if (e.detail.userInfo) {
-      app.globalData.userInfo = e.detail.userInfo
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      })
-    } else {
-      console.log('拒绝')
-    }
   },
 
   // 去个人中心
